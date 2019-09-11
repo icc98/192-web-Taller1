decd771ae8d2934c531a67000d4d0da1bd78a76e
Isabella Correa <isabelitacorreacarvajal@gmail.com>
2019-09-11 07:10:04 -0500

    cambios responsive

--- main.js
+++ main.js
@@ -40,3 +40,13 @@ if (black != null) {
     });
 }
 
+// Nuevo Js
+
+function menuRes() {
+	  var x = document.getElementById("myTopnav");
+	  if (x.className === "topnav") {
+		x.className += " responsive";
+	  } else {
+		x.className = "topnav";
+	  }
+}
