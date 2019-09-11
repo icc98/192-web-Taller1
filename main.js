f4eb606942bdbd12f0c88fc0d8b3faf9bc59c47d
Isabella Correa <isabelitacorreacarvajal@gmail.com>
2019-09-10 19:13:30 -0500

    Creación de Newsletter e interacción

---
+++ main.js
@@ -0,0 +1,42 @@
+imagen = document.querySelector(".interaction__right");
+
+silver = document.querySelector(".silver");
+gray = document.querySelector(".gray");
+blue = document.querySelector(".blue");
+black = document.querySelector(".black");
+
+
+imagenArray = [
+    "url('imagenes/opcion1.png')",
+    "url('imagenes/opcion2.png')",
+    "url('imagenes/opcion3.png')",
+    "url('imagenes/opcion4.png')",]
+
+if (silver != null) {
+
+    silver.addEventListener("click", (e) => {
+        imagen.style.backgroundImage = imagenArray[0];
+    });
+}
+
+if (gray != null) {
+
+    gray.addEventListener("click", (e) => {
+        imagen.style.backgroundImage = imagenArray[1];
+    });
+}
+
+if (blue != null) {
+
+    blue.addEventListener("click", (e) => {
+        imagen.style.backgroundImage = imagenArray[2];
+    });
+}
+
+if (black != null) {
+
+    black.addEventListener("click", (e) => {
+        imagen.style.backgroundImage = imagenArray[3];
+    });
+}
+
